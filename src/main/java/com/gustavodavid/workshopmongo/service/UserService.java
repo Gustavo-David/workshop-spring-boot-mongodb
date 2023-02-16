a71798127ea0f2ac1761339b27d8ff18673f709d
Gustavo Pinto <gustavodavidpinto@gmail.com>
2023-02-16 17:06:47 -0300

    Obtendo um usuário por id

--- src/main/java/com/gustavodavid/workshopmongo/service/UserService.java
+++ src/main/java/com/gustavodavid/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.gustavodavid.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gustavodavid.workshopmongo.domain.User;
 import com.gustavodavid.workshopmongo.repository.UserRepository;
+import com.gustavodavid.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 
         public List<User> findAll() {
                 return userRepository.findAll();
+        }
+
+        public User findById(String id){
+                Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
         }
 }
